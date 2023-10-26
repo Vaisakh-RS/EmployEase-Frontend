e94b12a399d107a5287aa30d4b9c8602b4f947e4
Ijas Iqbal <ijasiqbal4@gmail.com>
2023-10-27 02:37:09 +0530

    form updates backend

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -9,10 +9,10 @@ const EmployeeForm = () => {
     contactNumber: '',
     dateOfJoining: '',
     yearsOfExperience: '',
+    department:''
   });
 
   const [departments, setDepartments] = useState([]);
-  const [selectedDepartment,setSelectedDepartment]=useState('');
 
   const [isDateInput, setIsDateInput] = useState(false);
   const handleChange = (e) => {
@@ -23,30 +23,50 @@ const EmployeeForm = () => {
     });
   };
 
-  //to get the name of the departments to the select input
-  useEffect(()=>{
-    async function getDepartments() {
-      const apiUrl = 'https://employease-backend-production.up.railway.app/api/departments/';
+  function findExperience() {
+    const dateOfJoining = formData.dateOfJoining;
+    
+    if (dateOfJoining) {
+      const joinDate = new Date(dateOfJoining);
+      const currentDate = new Date();
   
-      try {
-        const response = await fetch(apiUrl, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json', // Adjust the content type as needed
-          },
-        });
+      // Calculate the difference in milliseconds
+      const difference = currentDate - joinDate;
   
-        if (response.ok) {
-          const responseData = await response.json();
-          setDepartments(responseData); // Update the state with the fetched departments
-        } else {
-          console.error('GET request failed:', response.status, response.statusText);
-        }
-      } catch (error) {
-        console.error('GET request error:', error);
-      }
+      // Calculate the number of years of experience
+      const yearsOfExperience = difference / (1000 * 60 * 60 * 24 * 365.25); // Approximate days in a year
+  
+      // Update the state with the calculated experience
+      setFormData({
+        ...formData,
+        yearsOfExperience: yearsOfExperience.toFixed(2), // You can adjust the decimal places as needed
+      });
     }
-  },[]);
+  }
+
+  async function getDepartments() {
+    const apiUrl = 'https://employease-backend-production.up.railway.app/api/departments/';
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json', // Adjust the content type as needed
+        },
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        setDepartments(responseData); // Update the state with the fetched departments
+      } else {
+        console.error('GET request failed:', response.status, response.statusText);
+      }
+    } catch (error) {
+      console.error('GET request error:', error);
+    }
+  }
+
+
   
 
   const handleDateInputFocus=()=>{
@@ -93,13 +113,12 @@ const EmployeeForm = () => {
       contactNumber: '',
       dateOfJoining: '',
       yearsOfExperience: '',
+      department:'',
     });
   };
 
  
-  const handleDepartmentChange = (event) => {
-    setSelectedDepartment(event.target.value);
-  };
+  
 
 
 return (
@@ -157,7 +176,10 @@ return (
                 type="date"
                 name="dateOfJoining"
                 value={formData.dateOfJoining}
-                onChange={handleChange}
+                onChange={(e)=>{
+                  handleChange(e);
+                  findExperience();
+                }}
                 required
                 placeholder='Date of Joining'
                 onFocus={handleDateInputFocus}
@@ -169,7 +191,10 @@ return (
                 type="text"
                 name="dateOfJoining"
                 value={formData.dateOfJoining}
-                onChange={handleChange}
+                onChange={(e)=>{
+                  handleChange(e);
+                  findExperience();
+                }}
                 required
                 placeholder='Date of Joining'
                 onFocus={handleDateInputFocus}
@@ -179,17 +204,18 @@ return (
             )}
           
         </div>
+        <p>Years of Experience:{formData.yearsOfExperience}</p>
 
         <div className='form-input'>
-          <select value={departments.name} onChange={handleDepartmentChange} className='border rounded p-2 w-70'>
+          <select name='department' value={formData.department} onChange={handleChange} onClick={getDepartments} className='border rounded p-2 w-70'>
             <option value="">Select Department</option>
             {departments.map((department, index) => (
-              <option key={index} value={department.name}>
+              <option key={index} value={department.id}>
                 {department.name}
               </option>
             ))}
           </select>
-          <p>Selected Department: {selectedDepartment}</p>
+          <p>Selected Department: {formData.department}</p>
         </div>
 
         <div>
