eb9daeb0e7d448574cb997b44d3c1d3017427de0
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-10-26 12:56:54 +0530

    Create employee form

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -1,9 +1,162 @@
-import { useState } from "react";
-import "../styles/employeeForm.css";
+import { useState } from 'react';
+import "../styles/employeeForm.css"
 
-export default function EmployeeForm()
-{
+const EmployeeForm = () => {
+  const [formData, setFormData] = useState({
+    employeeId: '',
+    name: '',
+    email: '',
+    contactNumber: '',
+    dateOfJoining: '',
+    yearsOfExperience: '',
+  });
+
+  const [isDateInput, setIsDateInput] = useState(false);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+//   const calculateExperience = () => {
+
+//     if (formData.dateOfJoining) {
+//       const joiningDate = new Date(formData.dateOfJoining);
+//       const currentDate = new Date();
+//       const experienceInMilliseconds = currentDate - joiningDate;
+//       const years = experienceInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+//       setFormData({
+//         ...formData,
+//         yearsOfExperience: years.toFixed(2),
+//       });
+//     }
+//   };
+
+
+  const handleDateInputFocus=()=>{
+    setIsDateInput(true);
+  }
+
+  const handleDateInputBlur = () => {
+    setIsDateInput(false);
+  };
+  //handlesubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const handleClear = () => {
+    setFormData({
+      employeeId: '',
+      name: '',
+      email: '',
+      contactNumber: '',
+      dateOfJoining: '',
+      yearsOfExperience: '',
+    });
+  };
+
+
+
+return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+      <div className="form-input">
+          <input
+            type="text"
+            name="employeeId"
+            value={formData.employeeId}
+            onChange={handleChange}
+            required
+            placeholder="Employee ID"
+            className="border rounded p-2"
+          />
+        </div>
+        <div className="form-input">
+          
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+            placeholder="Name"
+            className="border rounded p-2"
+          />
+        </div>
+        <div className="form-input">
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            placeholder='Email'
+            className="border rounded p-2"
+          />
+        </div>
+        <div className="form-input">
+          
+          <input
+            type="text"
+            name="contactNumber"
+            value={formData.contactNumber}
+            onChange={handleChange}
+            required
+            placeholder='Contact'
+            className="border rounded p-2"
+          />
+        </div>
+        <div className="form-input">
+            {isDateInput?(
+                <input
+                type="date"
+                name="dateOfJoining"
+                value={formData.dateOfJoining}
+                onChange={handleChange}
+                required
+                placeholder='Date of Joining'
+                onFocus={handleDateInputFocus}
+                onBlur={handleDateInputBlur}
+                className="border rounded p-2"
+              />
+            ):(
+                <input
+                type="text"
+                name="dateOfJoining"
+                value={formData.dateOfJoining}
+                onChange={handleChange}
+                required
+                placeholder='Date of Joining'
+                onFocus={handleDateInputFocus}
+                onBlur={handleDateInputBlur}
+                className="border rounded p-2"
+              />
+            )}
+          
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="form-button bg-green-500 text-white p-2 rounded"
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            onClick={handleClear}
+            className="form-button bg-red-500 text-white p-2 rounded"
+          >
+            Clear
+          </button>
+        </div>
+      </form>
+    </div>
+  );
   
-  
-}
-  
+};
+
+export default EmployeeForm;
