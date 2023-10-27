5c219100ff9fa53943e24102dd9efe764571554b
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-10-27 16:44:11 +0530

    Add styling to the ChooseManagerComponent

--- src/components/ChooseManagerComponent.jsx
+++ src/components/ChooseManagerComponent.jsx
@@ -1,74 +1,80 @@
-import "../styles/ChooseManager.css";
-import Fab from '@mui/material/Fab';
-import CloseIcon from '@mui/icons-material/Close';
 import { useState, useEffect } from 'react';
 
-const ChooseManagerComponent = ({selectedDepId,setChooseManager}) => {
+const ChooseManagerComponent = ({ selectedDepId, setChooseManager }) => {
+  const [employees, setEmployees] = useState([]);
 
-    const [employees, setEmployees] = useState([]);
+  function getEmployees() {
+    const apiUrl = 'https://employease-backend-production.up.railway.app/api/employees/';
 
-    function getEmployees() {
-        const apiUrl = 'https://employease-backend-production.up.railway.app/api/employees/';
-      
-        const requestOptions = {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json', // Adjust the content type as needed
-            },
-          };
-      
-        return fetch(apiUrl, requestOptions)
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error(`GET request failed: ${response.status} - ${response.statusText}`);
-            }
-            return response.json(); 
-          })
-          .then((data) => {
-              console.log('GET request successful:', data);
-              setEmployees(data); 
-            })
-          .catch((error) => {
-            console.error('GET request error:', error);
-            throw error; 
-          });
-      }
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json', // Adjust the content type as needed
+      },
+    };
 
-      useEffect(() => {
-        getEmployees();
-      }, []);
+    return fetch(apiUrl, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`GET request failed: ${response.status} - ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('GET request successful:', data);
+        setEmployees(data);
+      })
+      .catch((error) => {
+        console.error('GET request error:', error);
+        throw error;
+      });
+  }
 
-    return ( 
-        <div className="base">
-            <div className="closeicon">
-                <Fab
-                  color="error"
-                  aria-label="close"
-                  onClick={() => {
-                    setChooseManager(false);
-                }}
-                >
-                  <CloseIcon />
-                </Fab>
-            </div>
-            <h2>Eligible Employees</h2>
-            <div>
-              {employees
-                .filter(
-                  (employee) =>
-                    employee.department === selectedDepId &&
-                    employee.yearsOfExperience >= 5
-                )
-                .map((employee, index) => (
-                  <div className="employee" key={index}>
-                    <p>{employee.name}</p>
-                    <p>{employee.yearOfExperience}</p>
-                    <button>Assign as Manager</button>
-                  </div>
-                ))}
-            </div>
+  useEffect(() => {
+    getEmployees();
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
+      <div className="bg-white p-4 rounded-lg w-96">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-xl font-semibold">Eligible Employees</h2>
+          <button
+            onClick={() => {
+              setChooseManager(false);
+            }}
+            className="text-red-600 hover:text-red-800"
+          >
+            Close
+          </button>
         </div>
-     );
-}
- 
+        <div>
+          {employees
+            .filter(
+              (employee) =>
+                employee.department === selectedDepId &&
+                employee.yearsOfExperience >= 5
+            )
+            .map((employee, index) => (
+              <div
+                className="p-2 border border-gray-200 rounded-lg mb-2 flex justify-between items-center"
+                key={index}
+              >
+                <div>
+                  <p className="text-lg font-semibold">{employee.name}</p>
+                  <p>Experience: {employee.yearsOfExperience} years</p>
+                </div>
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600"
+                >
+                  Assign as Manager
+                </button>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default ChooseManagerComponent;
