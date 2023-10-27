f1502b3e9816ee77da4930ab2efb90a247c3951e
Irfan <95347450+Irfannnx@users.noreply.github.com>
2023-10-27 22:04:12 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './footer.css';
 
 const Footer = () => {
     return (
-        <footer style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+        <footer style={{width: '100%' }}>
             <p>&copy; 2021 Your Company Name. All rights reserved.</p>
             <p>Made By Ijas, Vaisakh, Irfan</p>
         </footer>
